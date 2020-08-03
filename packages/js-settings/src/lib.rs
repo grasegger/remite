6b0662062211e40e484c5baf311c212c149d0ac9
Karl Grasegger <51945878+grasegger@users.noreply.github.com>
2020-08-03 17:56:09 +0200

    Cleanup for the settings interface.

--- packages/js-settings/src/lib.rs
+++ packages/js-settings/src/lib.rs
@@ -1,19 +1,23 @@
 use wasm_bindgen::prelude::*;
+use serde_json::json;
+use std::collections::HashMap;
 
 #[wasm_bindgen]
 extern "C" {
-    #[wasm_bindgen(js_namespace= ["browser", "storage", "sync"], js_name = get)]
-    async fn getBrowserSettings() -> JsValue;
-    #[wasm_bindgen(js_namespace= ["browser", "storage", "sync"], js_name = set)]
-    async fn setBrowserSettings(values: &JsValue) -> JsValue;
+    #[wasm_bindgen(js_namespace= ["browser", "storage", "sync"], catch)]
+    pub async fn get() -> Result<JsValue, JsValue>;
+    #[wasm_bindgen(js_namespace= ["browser", "storage", "sync"], catch)]
+    pub async fn set(values: &JsValue) -> Result<JsValue, JsValue>;
+    #[wasm_bindgen(js_namespace = ["browser", "storage", "sync"], catch)]
+    pub async fn remove(key: &str) -> Result<JsValue, JsValue>;
 }
 
-pub async fn get() -> Result<JsValue, JsValue> {
-    let result = getBrowserSettings().await;
-    Ok(result)
+pub async fn set_map(key: &str, value: HashMap<String, String>) -> Result<JsValue, JsValue> {
+    let setting = JsValue::from_serde(&json!({ key: value })).unwrap();
+    set(&setting).await
 }
 
-pub async fn set(values: &JsValue) -> Result<JsValue, JsValue> {
-    let result = setBrowserSettings(values).await;
-    Ok(result)
+pub async fn set_string(key: &str, value: &str) -> Result<JsValue, JsValue> {
+    let setting = JsValue::from_serde(&json!({key: value})).unwrap();
+    set(&setting).await
 }
--- packages/remite-background/src/lib.rs
+++ packages/remite-background/src/lib.rs
@@ -1,14 +1,12 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 use settings;
-use serde_json::json;
 
 #[wasm_bindgen(start)]
 pub async fn main() -> Result<(), JsValue>{
-    let test_data = JsValue::from_serde(&json!({"abc": "def"})).unwrap();
-    settings::set(&test_data).await.unwrap();
+//    settings::set_string("test", "value").await.unwrap();
 
-    let result = settings::get().await;
+    let result = settings::get().await?;
     console::log_1(&format!("{:?}", result).into());
     Ok(())
 }
