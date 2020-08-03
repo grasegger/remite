553512f283f7b4ef0f3a8f0c75155e2f09528b5c
Karl Grasegger <51945878+grasegger@users.noreply.github.com>
2020-08-03 20:55:02 +0200

    Removed unneeded (for now) code.

--- packages/remite-background/src/lib.rs
+++ packages/remite-background/src/lib.rs
@@ -1,12 +1,8 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-use browser_storage_sync as settings;
 
 #[wasm_bindgen(start)]
 pub async fn main() -> Result<(), JsValue>{
-//    settings::set_string("test", "value").await.unwrap();
-
-    let result = settings::get().await?;
-    console::log_1(&format!("{:?}", result).into());
+    console::log_1(&"Welcome to the background js.".into());
     Ok(())
 }
